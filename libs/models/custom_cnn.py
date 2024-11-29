d38c81faaf1bbf8467f617021f2d9b329eaaed4d
Murad Huseynov <huseynovmurad2002@gmail.com>
2024-11-29 01:19:01 +0100

    4-report & 5

--- libs/models/custom_cnn.py
+++ libs/models/custom_cnn.py
@@ -8,7 +8,18 @@ class CustomCNN(nn.Module):
         ##############################
         ###     YOUR CODE HERE     ###
         ##############################  
-        # Define the convolutional layers
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(32 * 8 * 8, 256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, 10)
         
         pass
 
@@ -17,4 +28,15 @@ class CustomCNN(nn.Module):
         ###     YOUR CODE HERE     ###
         ##############################  
         # Define the forward pass
+
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+
+        x = x.view(x.size(0), -1)  
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
         return x
